9d6ffea4bcacefe78703cec25f624e4230e5caab
ankur12-1610 <76884959+ankur12-1610@users.noreply.github.com>
2021-05-30 13:53:47 +0530

    Img

--- utils/contributors.js
+++ utils/contributors.js
@@ -33,6 +33,33 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+
+    {
+        id: 2,
+        name: "Ankur Patil",
+        image: "/images/Ankur.jpg",
+        description: `
+            <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+            <div class="text-center">
+                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Learning
+                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Improving
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Contributing
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Eating
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+            </div>
+        `,
+        github: "https://github.com/ankur12-1610",
+    },
 ];
 
 export default CONTRIBUTERS;
