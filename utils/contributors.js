75a4e4c0cc7921b0c295a38d068c17c0661af7e1
somesh202 <77766761+somesh202@users.noreply.github.com>
2021-05-31 15:35:45 +0530

    Update contributors.js
    Changed handle_name, name, image and github id.

--- utils/contributors.js
+++ utils/contributors.js
@@ -8,8 +8,8 @@ const CONTRIBUTERS = [
     },
     // Start adding your data below
     {
-        handle_name: 'subodhk',
-        name: "Subodh Verma",
+        handle_name: 'somesh202',
+        name: "Somesh Ranjan",
         image: "/images/subodh.jpg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
@@ -31,7 +31,7 @@ const CONTRIBUTERS = [
                 <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
             </div>
         `,
-        github: "https://github.com/subodhk01",
+        github: "https://github.com/somesh202",
     },
 ];
 
