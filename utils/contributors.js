db8a5767c150efc8bbf5ca44d6a7a09e730ef71f
Divyansh Khetan <divyanshkhetan18@gmail.com>
2021-05-31 15:47:19 +0530

    changed the description

--- utils/contributors.js
+++ utils/contributors.js
@@ -37,26 +37,7 @@ const CONTRIBUTERS = [
         handle_name: 'divyanshkhetan',
         name: "Divyansh Khetan",
         image: "/images/autumn leaves.png",
-        description: `
-            <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
-            <div class="text-center">
-                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Chilling
-                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-                <br />
-                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Working
-                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-                <br />
-                <lord-icon src="https://cdn.lordicon.com/ribdawhq.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:250px;height:250px"></lord-icon> 
-                    Exploring
-                <lord-icon src="https://cdn.lordicon.com/ribdawhq.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:250px;height:250px"></lord-icon>
-                <br />
-                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Eating
-                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-            </div>
-        `,
+        description: "Keep grinding even if it is such a drag 😐",
         github: "https://github.com/divyanshkhetan",
     },
 ];
