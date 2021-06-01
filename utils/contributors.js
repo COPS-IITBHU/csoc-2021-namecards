b811848c3c662579216e561a23bbd59a959fe376
satakshi <satakshi.pal.civ19@itbhu.ac.in>
2021-06-01 19:04:16 +0530

    done sone formatting

--- utils/contributors.js
+++ utils/contributors.js
@@ -34,66 +34,35 @@ const CONTRIBUTERS = [
         github: "https://github.com/subodhk01",
     },
     {
-       
+
         handle_name: 'satakshi08',
         name: "Satakshi Pal",
         image: "/images/logo.jpg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
-            <lord-icon
-    src="https://cdn.lordicon.com/tdxypxgp.json"
-    trigger="loop"
-    colors="primary:#121331,secondary:#08a88a"
-    style="width:25px;height:25px">
-</lord-icon>
-Filming
-<lord-icon
-    src="https://cdn.lordicon.com/tdxypxgp.json"
-    trigger="loop"
-    colors="primary:#121331,secondary:#08a88a"
-    style="width:25px;height:25px">
-</lord-icon>
-                <br />
-                <lord-icon
-    src="https://cdn.lordicon.com/yalwfksd.json"
-    trigger="loop"
-    colors="primary:#121331,secondary:#08a88a"
-    style="width:25px;height:25px">
-</lord-icon>
-Protecting
-<lord-icon
-    src="https://cdn.lordicon.com/yalwfksd.json"
-    trigger="loop"
-    colors="primary:#121331,secondary:#08a88a"
-    style="width:25px;height:25px">
-</lord-icon><br />
-                <lord-icon
-    src="https://cdn.lordicon.com/smeqxwcv.json"
-    trigger="loop"
-    colors="primary:#121331,secondary:#08a88a"
-    style="width:25px;height:25px">
-</lord-icon>
-Vlogging
-<lord-icon
-    src="https://cdn.lordicon.com/smeqxwcv.json"
-    trigger="loop"
-    colors="primary:#121331,secondary:#08a88a"
-    style="width:25px;height:25px">
-</lord-icon><br />
-<lord-icon
-src="https://cdn.lordicon.com/lupuorrc.json"
-trigger="loop"
-colors="primary:#121331,secondary:#08a88a"
-style="width:25px;height:25px">
-</lord-icon>
-Enjoying
-<lord-icon
-    src="https://cdn.lordicon.com/lupuorrc.json"
-    trigger="loop"
-    colors="primary:#121331,secondary:#08a88a"
-    style="width:25px;height:25px">
-</lord-icon>
+            <lord-icon src="https://cdn.lordicon.com/tdxypxgp.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon>
+            Filming
+            <lord-icon src="https://cdn.lordicon.com/tdxypxgp.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon>
+            <br />
+            <lord-icon src="https://cdn.lordicon.com/yalwfksd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon>
+            Protecting
+            <lord-icon src="https://cdn.lordicon.com/yalwfksd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon><br />
+            <lord-icon src="https://cdn.lordicon.com/smeqxwcv.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon>
+            Vlogging
+            <lord-icon src="https://cdn.lordicon.com/smeqxwcv.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon>
+            <br />
+            <lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon>
+            Enjoying
+            <lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px">
+            </lord-icon>
             </div>
         `,
         github: "https://github.com/satakshi08",
