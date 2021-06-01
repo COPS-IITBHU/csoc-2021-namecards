2bde82255649ce17676a80495a39fd0db94ecf7b
Your Name <you@example.com>
2021-06-01 13:37:54 +0530

    added my card

--- utils/contributors.js
+++ utils/contributors.js
@@ -33,6 +33,76 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+    {
+        handle_name: 'soumyo123',
+        name: "Soumyajyoti Dey",
+        image: "/images/soumyo123.jpg",
+        description: `
+            <div class = "text-center">
+                <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+                
+                <lord-icon
+                    src="https://cdn.lordicon.com//ktxpktdd.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                Gaming
+                <lord-icon
+                    src="https://cdn.lordicon.com//ktxpktdd.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                <br />
+
+                <lord-icon
+                    src="https://cdn.lordicon.com//zzcjjxew.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                Travelling
+                <lord-icon
+                    src="https://cdn.lordicon.com//zzcjjxew.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                <br />
+
+                <lord-icon
+                    src="https://cdn.lordicon.com//anihkfxp.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                Coding
+                <lord-icon
+                    src="https://cdn.lordicon.com//anihkfxp.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                <br />
+
+                <lord-icon
+                    src="https://cdn.lordicon.com//mmspidej.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                Music
+                <lord-icon
+                    src="https://cdn.lordicon.com//mmspidej.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+            </div>
+        `,
+        github: "https://github.com/soumyo123-prog",
+    }
 ];
 
 export default CONTRIBUTERS;
