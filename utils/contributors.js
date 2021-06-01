ba6113b0434d340c9aedbe88110c21c687f252af
Danish Ahmed Mirza <77742477+try-catch-stack@users.noreply.github.com>
2021-06-01 13:53:19 +0530

    Add my card details

--- utils/contributors.js
+++ utils/contributors.js
@@ -33,6 +33,35 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+    {
+        handle_name: 'Danish',
+        name: "Danish Ahmed Mirza",
+        image: "/images/Danish.png",
+        description: `
+            <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+            <div class="text-center">
+            
+                <lord-icon src="https://cdn.lordicon.com//anihkfxp.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Coding
+                <lord-icon src="https://cdn.lordicon.com//anihkfxp.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//qhgmphtg.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Exploring
+                <lord-icon src="https://cdn.lordicon.com//qhgmphtg.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//ktxpktdd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Gaming
+                <lord-icon src="https://cdn.lordicon.com//ktxpktdd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//mmspidej.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Music
+                <lord-icon src="https://cdn.lordicon.com//mmspidej.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+
+                </div>
+                
+        `,
+        github: "https://github.com/try-catch-stack",
+    },
 ];
 
 export default CONTRIBUTERS;
