6a5aa4ec8b4e6c4a5bf2027a6aba07c79427a320
puranjayK <puranjay01@gmail.com>
2021-06-01 14:43:21 +0530

    Added name and image

--- utils/contributors.js
+++ utils/contributors.js
@@ -36,7 +36,7 @@ const CONTRIBUTERS = [
     {
         handle_name: 'puranjayK',
         name: "Puranjay Khanijo",
-        image: "/images/puranjay.jpg",
+        image: "/images/penguin.jpg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
