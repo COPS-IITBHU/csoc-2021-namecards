2cf873cf39496029d5f17bf6223645bed9c1de21
amit-shew <amit98kp@gmail.com>
2021-06-02 16:13:28 +0530

    changes in js

--- utils/contributors.js
+++ utils/contributors.js
@@ -6,7 +6,6 @@ const CONTRIBUTERS = [
         description: "Hello everyone, this is the first card. Go ahead, add your own!",
         github: "https://github.com/COPS-IITBHU",
     },
-    // Start adding your data below
     {
         handle_name: 'subodhk',
         name: "Subodh Verma",
@@ -33,6 +32,15 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+    {
+        handle_name: 'amit-shew',
+        name: "My First Card",
+        image: "/images/amit-shew.jpg",
+        description: "Hello everyone, this is the first card by me :)",
+        github: "https://github.com/amit-shew",
+    },
+    
+    
 ];
 
 export default CONTRIBUTERS;
