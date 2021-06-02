1edd6f9267347e18a3df2ff6bc6ea59f9a62b2f4
JAYESH DEWANGAN <jyshdewangan@gmail.com>
2021-06-02 12:54:16 +0530

    New one lol

--- utils/contributors.js
+++ utils/contributors.js
@@ -43,19 +43,19 @@ const CONTRIBUTERS = [
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
                 <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Chilling
+                    Chimling
                 <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
                 <br />
                 <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Working
+                    comding
                 <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
                 <br />
                 <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Playing
+                    Playming
                 <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
                 <br />
                 <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Eating
+                    Eatming
                 <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
             </div>
         `,
