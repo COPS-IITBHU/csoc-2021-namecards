eb565de30d1be2139dc579cce2e70991b9531def
Madhav <madhav.jhunjhunwala.cd.mec19@itbhu.ac.in>
2021-06-03 09:36:18 +0530

    Added my details in namecard

--- utils/contributors.js
+++ utils/contributors.js
@@ -1,16 +1,10 @@
 const CONTRIBUTERS = [
-    {
-        handle_name: 'cops_iitbhu',
-        name: "First Card",
-        image: "/images/cops.jpg",
-        description: "Hello everyone, this is the first card. Go ahead, add your own!",
-        github: "https://github.com/COPS-IITBHU",
-    },
+    
     // Start adding your data below
     {
-        handle_name: 'subodhk',
-        name: "Subodh Verma",
-        image: "/images/subodh.jpg",
+        handle_name: '420-Madhav',
+        name: "Madhav Jhunjhunwala",
+        image: "/images/CSoc_image.jpg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
@@ -31,8 +25,16 @@ const CONTRIBUTERS = [
                 <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
             </div>
         `,
-        github: "https://github.com/subodhk01",
+        github: "https://github.com/420-Madhav",
+    },
+    {
+        handle_name: 'cops_iitbhu',
+        name: "First Card",
+        image: "/images/cops.jpg",
+        description: "Hello everyone, this is the first card. Go ahead, add your own!",
+        github: "https://github.com/COPS-IITBHU",
     },
 ];
+
 
 export default CONTRIBUTERS;
