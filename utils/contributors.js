9e67eebd2a0bae5b6c21ef3aa0e9e9d0d329d528
lelouch-of-the-code <1aniruddhpatil.01@gmail.com>
2021-06-04 07:40:23 +0530

    my commit

--- utils/contributors.js
+++ utils/contributors.js
@@ -33,6 +33,42 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+    {
+        handle_name: 'lelouch-of-the-code',
+        name: "Aniruddh Patil",
+        image: "/images/ani.jpg",
+        description: `
+        <script src="https://cdn.lordicon.com/libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+<div class="text-center">
+ <lord-icon
+    src="https://cdn.lordicon.com/ochimkct.json"
+    trigger="loop"
+    colors="primary:#121331,secondary:#08a88a"
+    style="width:35px;height:35px">
+</lord-icon>
+  Learning
+   <lord-icon
+    src="https://cdn.lordicon.com/ochimkct.json"
+    trigger="loop"
+    colors="primary:#121331,secondary:#08a88a"
+    style="width:35px;height:35px">
+</lord-icon><br>
+ <lord-icon
+    src="https://cdn.lordicon.com/ybfcwnqv.json"
+    trigger="loop"
+    colors="primary:#121331,secondary:#08a88a"
+    style="width:35px;height:35px">
+</lord-icon>
+  Git
+ <lord-icon
+    src="https://cdn.lordicon.com/ybfcwnqv.json"
+    trigger="loop"
+    colors="primary:#121331,secondary:#08a88a"
+    style="width:35px;height:35px">
+</lord-icon>
+</div>`,
+        github: "https://github.com/lelouch-of-the-code",
+    },
 ];
 
 export default CONTRIBUTERS;
