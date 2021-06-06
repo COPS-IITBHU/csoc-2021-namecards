d682e62347ce28dc611d3b9aeb15d5c210094abd
amansingh1906 <singhamankumar98@gmail.com>
2021-06-06 16:58:21 +0530

    mycard

--- utils/contributors.js
+++ utils/contributors.js
@@ -8,30 +8,31 @@ const CONTRIBUTERS = [
     },
     // Start adding your data below
     {
-        handle_name: 'subodhk',
-        name: "Subodh Verma",
-        image: "/images/subodh.jpg",
+        handle_name: 'dev-it-aman',
+        name: "Aman Kumar",
+        image: "/images/nick.jpg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
-                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Chilling
-                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+               <lord-icon src="https://cdn.lordicon.com/anihkfxp.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                Web Development!
+                <lord-icon  src="https://cdn.lordicon.com/anihkfxp.json"  trigger="loop"  colors="primary:#121331,secondary:#08a88a"  style="width:25px;height:25px"></lord-icon>
                 <br />
-                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Working
-                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <lord-icon  src="https://cdn.lordicon.com/ptbbcteb.json"  trigger="loop" colors="primary:#121331,secondary:#08a88a"  style="width:25px;height:25px"></lord-icon>
+                Competitive Programming!
+                <lord-icon  src="https://cdn.lordicon.com/ptbbcteb.json"  trigger="loop"  colors="primary:#121331,secondary:#08a88a"  style="width:25px;height:25px"></lord-icon>
+                  <br />
+                  <lord-icon  src="https://cdn.lordicon.com/mmspidej.json"  trigger="loop"  colors="primary:#121331,secondary:#08a88a"  style="width:25px;height:25px"></lord-icon>
+                  Listening Music!
+                  <lord-icon  src="https://cdn.lordicon.com/mmspidej.json"  trigger="loop"  colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
                 <br />
-                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Playing
-                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-                <br />
-                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Eating
-                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-            </div>
+                <lord-icon src="https://cdn.lordicon.com/jpdtnwas.json"  trigger="loop"  colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                   Eating!!
+                <lord-icon src="https://cdn.lordicon.com/jpdtnwas.json" trigger="loop"  colors="primary:#121331,secondary:#08a88a"  style="width:25px;height:25px"></lord-icon>
+
+                </div>
         `,
-        github: "https://github.com/subodhk01",
+        github: "https://github.com/dev-it-aman",
     },
 ];
 
