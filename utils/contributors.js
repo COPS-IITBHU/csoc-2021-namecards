023ff84b04a8f8d50375f7f87ba4dd548c57f4f9
UJ5 <ujjawalmodamwal325@gmail.com>
2021-06-06 20:06:25 +0530

    namecards changed and image added

--- utils/contributors.js
+++ utils/contributors.js
@@ -33,6 +33,32 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+    {
+        handle_name: 'ujjawalmodanwal',
+        name: "Ujjawal Modanwal",
+        image: "/images/Ujjawal_M.jpg",
+        description: `
+            <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+            <div class="text-center">
+                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Chilling
+                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Working
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Playing
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Eating
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+            </div>
+        `,
+        github: "https://github.com/ujjawalmodanwal",
+    },
 ];
 
 export default CONTRIBUTERS;
