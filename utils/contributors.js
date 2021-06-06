556f5c3e9059acbf3650ba6da9ca4360815fc5da
chitrankiit20 <77741010+chitrankiit20@users.noreply.github.com>
2021-06-06 20:46:39 +0530

    Update contributors.js

--- utils/contributors.js
+++ utils/contributors.js
@@ -8,9 +8,9 @@ const CONTRIBUTERS = [
     },
     // Start adding your data below
     {
-        handle_name: 'subodhk',
+        handle_name: 'trek',
         name: "Chitrank Agarwal",
-        image: "/images/subodh.jpg",
+        image: "/images/chitranka.jpg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
@@ -31,7 +31,7 @@ const CONTRIBUTERS = [
                 <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
             </div>
         `,
-        github: "https://github.com/subodhk01",
+        github: "https://github.com/chitrankiit20",
     },
 ];
 
