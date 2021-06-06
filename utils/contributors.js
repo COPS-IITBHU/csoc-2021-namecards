b24bdabf2e3722ab35a64ab3561ee9e927bb6f39
Pallavi-1108 <pallavi.student.eee20@iitbhu.ac.in>
2021-06-06 13:31:30 +0530

    added namecard

--- utils/contributors.js
+++ utils/contributors.js
@@ -33,6 +33,13 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+    {
+        handle_name: 'Pallavi',
+        name: "Pallavi",
+        image: "C:\Users\manik\Documents\GitHub\csoc-2021-namecards\public\images\img.jpeg",
+        description: "Hello everyone!",
+        github: "https://github.com/Pallavi-1108",
+    },
 ];
 
 export default CONTRIBUTERS;
