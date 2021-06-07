dd3e9a5ceb229fee29f2cf572800405a9fe1cfaa
Kalpitadhao77 <82868584+Kalpitadhao77@users.noreply.github.com>
2021-06-07 11:13:10 +0530

    Update contributors.js

--- utils/contributors.js
+++ utils/contributors.js
@@ -8,7 +8,9 @@ const CONTRIBUTERS = [
     },
     // Start adding your data below
     {
-    
+        handle_name: 'subodhk',
+        name: "Subodh Verma",
+        image: "/images/subodh.jpg",
         description: `
             <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
             <div class="text-center">
@@ -28,14 +30,13 @@ const CONTRIBUTERS = [
                     Eating
                 <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
             </div>
-        `,
-        github: "https://github.com/Kalpitadhao77",
+        `, github: "https://github.com/subodhk01",
     },
   {
       handle_name: 'Kalpitadhao77',
-        name: "Kalpit Adhao",
-        image: "/images/untitled.jpg",
-        image: "/images/Kalpit.jpg",
+      name: "Kalpit Adhao",
+      image: "/images/Kalpit.jpg",
+      github: "https://github.com/Kalpitadhao77",
   },
     
 ];
