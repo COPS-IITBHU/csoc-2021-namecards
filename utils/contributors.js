1128749efe7b7ccf53295ee55e7869c94bb014ae
Gourav Kumar <67992259+Gourav-Kr@users.noreply.github.com>
2021-06-08 01:51:41 +0530

    Updated to add Namecard

--- utils/contributors.js
+++ utils/contributors.js
@@ -33,6 +33,34 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/subodhk01",
     },
+
+    // Mine
+    {
+        handle_name: 'Gourav-Kr',
+        name: "Gourav Kumar",
+        image: "/images/abfd8b6d21eddc87078ca9d296c4e6cb.jpg",
+        description: `
+            <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+            <div class="text-center">
+                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Chilling
+                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Focusing
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Valoranting
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Foodin
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+            </div>
+        `,
+        github: "https://github.com/Gourav-Kr",
+    },
 ];
 
 export default CONTRIBUTERS;
