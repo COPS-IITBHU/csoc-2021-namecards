2330f0dfd7b85c9d7adcd912de394130ac05232f
Manoj <manoj.kumargupta.ece20@itbhu.ac.in>
2021-09-11 16:42:56 +0530

    Manoj Gupta made changes

--- utils/contributors.js
+++ utils/contributors.js
@@ -8,30 +8,11 @@ const CONTRIBUTERS = [
     },
     // Start adding your data below
     {
-        handle_name: 'subodhk',
-        name: "Subodh Verma",
-        image: "/images/subodh.jpg",
-        description: `
-            <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
-            <div class="text-center">
-                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Chilling
-                <lord-icon src="https://cdn.lordicon.com//gmzxduhd.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-                <br />
-                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Working
-                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-                <br />
-                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Playing
-                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-                <br />
-                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
-                    Eating
-                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
-            </div>
-        `,
-        github: "https://github.com/subodhk01",
+        handle_name: 'MKG2002',
+        name: "Manoj Kumar Gupta",
+        image: "/images/Myimage.jpg",
+        description: "Hello Everybody, this is MKG2002's namecard",
+        github: "https://github.com/MKG2002",
     },
 ];
 
