43bc710ce3d346a588b6099b306dd2a48180a483
tsari02 <saritesh.tiwari.cse20@itbhu.ac.in>
2021-10-22 18:25:42 +0530

    namecard full

--- utils/contributors.js
+++ utils/contributors.js
@@ -13,24 +13,39 @@ const CONTRIBUTERS = [
         image: "/images/yoshitake.jpg",
         description: `
             <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
+            <div class="text-center">
+                <lord-icon
+                    src="https://cdn.lordicon.com/wxnxiano.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
+                IIT BHU CSE SOPHOMORE
+                <lord-icon
+                    src="https://cdn.lordicon.com/wxnxiano.json"
+                    trigger="loop"
+                    colors="primary:#121331,secondary:#08a88a"
+                    style="width:25px;height:25px">
+                </lord-icon>
 
-            <lord-icon
-                src="https://cdn.lordicon.com/wxnxiano.json"
-                trigger="loop"
-                colors="primary:#121331,secondary:#08a88a"
-                style="width:250px;height:250px">
-            </lord-icon>
-            IIT BHU CSE SOPHOMORE
-            <lord-icon
-                src="https://cdn.lordicon.com/wxnxiano.json"
-                trigger="loop"
-                colors="primary:#121331,secondary:#08a88a"
-                style="width:250px;height:250px">
-            </lord-icon>
+                <br />
+
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Working
+                <lord-icon src="https://cdn.lordicon.com//xxtkutpo.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Playing
+                <lord-icon src="https://cdn.lordicon.com//pndvzexs.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon> 
+                    Eating
+                <lord-icon src="https://cdn.lordicon.com//jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>
 
 
 
-            
+
+            </div>
             `,
         
         
